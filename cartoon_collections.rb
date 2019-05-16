853a3bad498deab1f9ed37a748ce1d21102e90c0
Rj <rikrdo.ortega@gmail.com>
2019-05-15 20:39:21 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,19 +10,19 @@ def long_planeteer_calls(array_call)
   array_call.any? {|name| name.length > 4}
 end
 
-def find_the_cheese(array)
-  cheeses = ["cheddar", "gouda", "camembert"]
-  array.any? do |i| 
-    if cheeses.include?(i)
-      return i
-    end
-  end
-  return nil
-end
-
-# def find_the_cheese(potentially_cheesy_items)
-#   cheeses = %w[gouda cheddar camembert]
-#   potentially_cheesy_items.find do |maybe_cheese|
-#     cheeses.include?(maybe_cheese)
+# def find_the_cheese(array)
+#   cheeses = ["cheddar", "gouda", "camembert"]
+#   array.any? do |i| 
+#     if cheeses.include?(i)
+#       return i
+#     end
 #   end
+#   return nil
 # end
+
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
