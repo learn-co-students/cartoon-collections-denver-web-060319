431dc310dd98bf0d9bef6c8f8311c1f7e8bf1ac6
“Patrick <pctennislessons@gmail.com>
2019-05-17 16:21:23 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |element, index|
+    print "#{index +1}. #{element}"
+  end  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(fruits)
+  fruits.collect do |element|
+    element.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(members)
+  members.any? {|i| i.length > 4} 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(shopping_list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  shopping_list.each do |element|
+    if cheese_types.include? element  
+      return element 
+    end
+  end
+  shopping_list.each do |item|
+    if !cheese_types.include? item
+      return nil
+    end
+  end
 end
